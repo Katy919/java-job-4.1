88567f64e7be3c730e660bf73ab40a9522a0551b
Ekaterina Golubeva <ekaterinagolybeva94@gmail.com>
2025-09-29 21:24:02 +0300

    Gitignore added

--- src/test/java/ru/netology/services/CountServiceTest.java
+++ src/test/java/ru/netology/services/CountServiceTest.java
@@ -1,33 +1,25 @@
 package ru.netology.services;
 
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.CsvSource;
 
 public class CountServiceTest {
 
-    @Test
-    public void testCalculateCountMonths() {
+    @ParameterizedTest
+    //@CsvSource({
+    //    "3,3000,10000,20000",
+    //    "2,60000,100000,150000"
+    //})
+    @CsvFileSource(files = "src/test/resources/count.csv")
+    public void testCalculateCountMonths(int expected, int expenses, int income, int threshold) {
         CountService service = new CountService();
 
-        int expected = 3;
-        int expenses = 3_000;
-        int income = 10_000;
-        int threshold = 20_000;
-        // вызываем целевой метод:
-        int actual = service.calculate(income, expenses, threshold);
-
-        // производим проверку (сравниваем ожидаемый и фактический):
-        Assertions.assertEquals(actual, expected);
-    }
-
-    @Test
-    public void testCalculateCountPonths() {
-        CountService service = new CountService();
-
-        int expected = 2;
-        int expenses = 60_000;
-        int income = 100_000;
-        int threshold = 150_000;
+        // int expected = 3;
+        //  int expenses = 3_000;
+        //  int income = 10_000;
+        // int threshold = 20_000;
         // вызываем целевой метод:
         int actual = service.calculate(income, expenses, threshold);
 
@@ -35,3 +27,19 @@ public class CountServiceTest {
         Assertions.assertEquals(actual, expected);
     }
 }
+
+
+//@Test
+//public void testCalculateCountPonths() {
+//  CountService service = new CountService();
+
+//int expected = 2;
+//int expenses = 60_000;
+//int income = 100_000;
+//int threshold = 150_000;
+// вызываем целевой метод:
+//int actual = service.calculate(income, expenses, threshold);
+
+// производим проверку (сравниваем ожидаемый и фактический):
+//Assertions.assertEquals(actual, expected);
+
